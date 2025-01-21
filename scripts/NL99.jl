5545ba06a96aa2387aa19fee338a7311d4798d9b
Nina De La Torre <kneenaugh@wireless-10-148-223-27.public.utexas.edu>
2025-01-21 15:30:00 -0600

    Added Nelson plots

--- scripts/NL99.jl
+++ scripts/NL99.jl
@@ -116,37 +116,44 @@ print("\nTime to create the simplified problem:")
 print("\nTime to solve the simplified Nelson reaction system with Rodas4(): ")
 @time solve(prob, Rodas4());
 
-
-
-
+### Plotting ###
 # C and C+
-plot(sol, idxs = (0,6), lw = 3, lc = "blue", title = "Nelson")
-plot!(sol, idxs = (0,12), lw = 3, lc = "orange")
+plot(sol, idxs = (0,6), lw = 3, lc = "blue")
+plot!(sol, idxs = (0,12), lw = 3, lc = "orange", title = "Nelson: Abundance of C and C+")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/C_and_Cp_Nelson.png")
 
-#=
+
 # CO
-plot(sol, idxs = (0,10), lw = 3, lc = "green", title = "Nelson")
+plot(sol, idxs = (0,10), lw = 3, lc = "green", title = "Nelson: Abundance of CO")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/CO_Nelson.png")
 
 # O 
-plot(sol, idxs = (0,8), lw = 3, lc = "blue", title = "Nelson")
+plot(sol, idxs = (0,8), lw = 3, lc = "blue", title = "Nelson: Abundance of O")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/CO_Nelson.png")
 
 # He+
-plot(sol, idxs = (0,4), lw = 3, lc = "light pink", title = "Nelson")
+plot(sol, idxs = (0,4), lw = 3, lc = "light pink", title = "Nelson: Abundance of He+")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/Hep_Nelson.png")
 
 # CH and CH2 = CHx
-plot(sol, idxs = (0,7), lw = 3, lc = "blue", title = "Nelson")
+plot(sol, idxs = (0,7), lw = 3, lc = "blue", title = "Nelson: CHx")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/CH_and_CH2_Nelson.png")
 
 # OH, OH+, H2O, H2O+, and O2 = OHx
-plot(sol, idxs = (0,9), lw = 3, lc = "green", title = "Nelson")
+plot(sol, idxs = (0,9), lw = 3, lc = "green", title = "Nelson: OHx")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/OH_OHp_H2O_H2Op_O2_Nelson.png")
 
 # H3+
 plot(sol, idxs = (0,2), lw = 3, lc = "orange", title = "Nelson")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/H3p_Nelson.png")
 
 # HCO+ 
 plot(sol, idxs = (0,11), lw = 3, lc = "orange", title = "Nelson")
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/HCOp_Nelson.png")
 
 # M 
 plot(sol, idxs = (0,14), lw = 3, lc = "light blue", title = "Nelson")
-=#
+savefig("/Users/kneenaugh/Desktop/Git/AstroChemNetwork/plots/Mg_Fe_Na_Nelson.png")
+
 
 
